8c39c2be2d723a5fa36c6d04a8ea8536187615dd
kauanrodrigues14 <kauanoddy@gmail.com>
2024-06-05 00:04:13 -0300

    Mapa starter/Sensor page 100%

--- app/src/main/java/com/example/projetojuquery/InicioFragment.kt
+++ app/src/main/java/com/example/projetojuquery/InicioFragment.kt
@@ -2,48 +2,47 @@ package com.example.projetojuquery
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 
 
 class InicioFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_inicio, container, false)
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val contexto = requireContext()
-        val edtSenha = view.findViewById<EditText>(R.id.edtSenhaAdm).toString()
-        val edtLogin = view.findViewById<EditText>(R.id.edtLoginAdm).toString()
-        val btnLogin = view.findViewById<Button>(R.id.btnLoginAdm)
+        val edtSenhaAdm = view.findViewById<EditText>(R.id.edtSenhaAdm)
+        val edtLoginAdm = view.findViewById<EditText>(R.id.edtLoginAdm)
+        val btnLoginAdm = view.findViewById<Button>(R.id.btnLoginAdm)
 
-        btnLogin.setOnClickListener {
-            if (edtLogin == "admin" || edtSenha == "0000") {
+        btnLoginAdm.setOnClickListener {
+            val login = edtLoginAdm.text.toString()
+            val senha = edtSenhaAdm.text.toString()
 
+            if (login == "admin" && senha == "adm") {
                 Toast.makeText(contexto, "Login bem-sucedido!", Toast.LENGTH_SHORT).show()
-                val intent = Intent(contexto, PaginaInicial::class.java)
+                val intent = Intent(contexto, CRUD::class.java)
                 startActivity(intent)
-
-            }
-            else if(edtLogin.isEmpty() || edtSenha.isEmpty()){
-                Toast.makeText(contexto, "Login e senha não podem estar vazios.", Toast.LENGTH_SHORT).show()
-        }else{
-                Toast.makeText(contexto, "Login ou Senha Inválidos.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(contexto, "Credenciais inválidas.", Toast.LENGTH_SHORT).show()
             }
         }
-
+    }
 
     }
-}
+
