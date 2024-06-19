e6b7b0484515a1945e6321939f7581a52af1faf6
kauanrodrigues14 <kauanoddy@gmail.com>
2024-06-19 14:23:20 -0300

    Projeto Juquery 2.0

--- app/src/main/java/com/example/projetojuquery/PaginaInicial.kt
+++ app/src/main/java/com/example/projetojuquery/PaginaInicial.kt
@@ -3,48 +3,44 @@ package com.example.projetojuquery
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
+import androidx.fragment.app.Fragment
+
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class PaginaInicial : AppCompatActivity() {
-
     private lateinit var barraNavegacao: BottomNavigationView
-    private lateinit var viewPager: ViewPager2
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_pagina_inicial)
 
-        viewPager = findViewById(R.id.viewPager)
+
         barraNavegacao = findViewById(R.id.navView)
 
-        val adapter = ViewPagerAdapter(this)
-        viewPager.adapter = adapter
-
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                barraNavegacao.menu.getItem(position).isChecked = true
-            }
-        })
-
         barraNavegacao.setOnItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
+            when(menuItem.itemId){
                 R.id.Início -> {
-                    viewPager.currentItem = 0
+                    substuirFragment(InicioFragment())
                     true
                 }
                 R.id.sensor -> {
-                    viewPager.currentItem = 1
+                    substuirFragment(SensorFragment())
                     true
                 }
                 R.id.Clima -> {
-                    viewPager.currentItem = 2
+                    substuirFragment(ClimaFragment())
                     true
                 }
                 else -> false
             }
         }
+        substuirFragment(InicioFragment())
+
+    }
+    private fun substuirFragment(fragment : Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+
     }
 }
